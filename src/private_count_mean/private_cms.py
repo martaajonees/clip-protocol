72b64f480ed2b21b95f21f7d343c837e168c2cb5
Marta <marta.jonesgonzalez@alum.uca.es>
2024-12-03 20:40:32 +0100

    Modifying algorithms

--- src/private_count_mean/private_cms.py
+++ src/private_count_mean/private_cms.py
@@ -9,7 +9,7 @@ from progress.bar import Bar
 from tabulate import tabulate
 import sys
 
-# Enlace con la ruta para las utilidades (funciones de uso comun)
+# Link with the route for the utilities (common functions)
 file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..',  'utils', 'utils.py'))
 module_name = 'utils'
 
@@ -28,17 +28,20 @@ class privateCMS:
         self.domain = domain
         self.N = len(dataset)
 
-        # Creación de la matriz de sketch
+        # Creation of the sketch matrix
         self.M = np.zeros((self.k, self.m))
 
-        # Definimos la familia de hashes independientes 3 a 3
-        primos = list(primerange(10**6, 10**7))
-        p = primos[random.randint(0, len(primos)-1)]
+        # List to store the privatized matrices
+        self.client_matrix = []
+
+        # Definition of the hash family 3 by 3
+        primes = list(primerange(10**6, 10**7))
+        p = primes[random.randint(0, len(primes)-1)]
         self.H = utils.generate_hash_functions(self.k,p, 3,self.m)
     
     def bernoulli_vector(self):
         b = np.random.binomial(1, (np.exp(self.epsilon/2)) / ((np.exp(self.epsilon/2)) + 1), self.m)
-        b = 2 * b - 1  #Convertir los 0s en -1s
+        b = 2 * b - 1  # Convert 0 to -1
         return b
 
     def client(self,d):
@@ -48,89 +51,73 @@ class privateCMS:
         v[selected_hash(d)] = 1
         b = self.bernoulli_vector()
         v_aux = v*b
-        # matriz de vs las vamos almacenando y la j
+        # Store the privatized matrix
+        self.client_matrix.append((v_aux,j))
+        
         return v_aux,j
 
-    def actualizar_matriz_sketch(self,v,j):
+    def update_sketch_matrix(self,v,j):
         c_e = (np.exp(self.epsilon/2)+1) / ((np.exp(self.epsilon/2))-1)
         x = self.k * ((c_e/2) * v + (1/2) * np.ones_like(v))
         for i in range (self.m):
-            self.M[j,i] =  self.M[j,i] + x[i]
+            self.M[j,i] += x[i]
 
-    def estimar_d(self,d):
+    def estimate_d(self,d):
         sum_aux = 0
         for i in range(self.k):
             selected_hash = self.H[i]
-            sum_aux += self.M[i,selected_hash(d)]
+            sum_aux += self.M[i, selected_hash(d)]
         
-        f_estimada = (self.m/(self.m-1))*((sum_aux/self.k)-(self.N/self.m))
-        return f_estimada
+        f_estimated = (self.m/(self.m-1))*((sum_aux/self.k)-(self.N/self.m))
+        return f_estimated
     
-    def execute(self):
-        bar = Bar('Procesando datos de los clientes', max=len(self.dataset), suffix='%(percent)d%%')
-        t_cliente = 0
-        t_act = 0
-        size_cliente = 0
+    def execute_client(self):
+        bar = Bar('Processing client data', max=len(self.dataset), suffix='%(percent)d%%')
+        privatized_data = []
         for d in self.dataset:
-            inicio = time.time()
             v_i, j_i = self.client(d)
-            fin = time.time()
-            t_cliente += (fin - inicio) * 1000
-            size_cliente = sys.getsizeof(v_i) + sys.getsizeof(j_i)
-
-            inicio = time.time()
-            self.actualizar_matriz_sketch(v_i,j_i)
-            fin = time.time()
-            t_act += (fin - inicio) * 1000
+            privatized_data.append((v_i,j_i))
             bar.next()
         bar.finish()
-        t_cliente = t_cliente/len(self.dataset)
-        t_act = t_act/len(self.dataset)
+        return privatized_data
+    
+    def execute_server(self,privatized_data):
+        bar = Bar('Update sketch matrix', max=len(privatized_data), suffix='%(percent)d%%')
+        for data in privatized_data:
+            self.update_sketch_matrix(data[0],data[1])
+            bar.next()
+        bar.finish()
 
-        F_estimada = {}
-        t_esti = 0
-        bar = Bar('Obteniendo histograma de frecuencias estimadas', max=len(self.domain), suffix='%(percent)d%%')
+        F_estimated = {}
         for x in self.domain:
-            inicio = time.time()
-            F_estimada[x] = self.estimar_d(x)
-            fin = time.time()
-            t_esti += (fin - inicio) * 1000
+            F_estimated[x] = self.estimate_d(x)
             bar.next()
         bar.finish()
-        t_esti = t_esti/len(self.domain)
-        
-        # Tabla de tiempos de ejecución
-        tiempos = [['Cliente (Por usuario)', str("{:.4f}".format(t_cliente)) + ' ms'],['Servidor (Actualizar matriz)',str("{:.4f}".format(t_act)) + ' ms'],['Servidor (Estimación individual)',str("{:.4f}".format(t_esti)) + ' ms'],['Ancho de banda',str("{:.3f}".format(size_cliente)) + ' kB']]
-
-        return F_estimada, tiempos
-
+        return F_estimated
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Algoritmo Private Count Mean Sketch para la estimación de frecuencias a partir de un dominio conocido.")
-    parser.add_argument("-k", type=int, required=True, help="Numero de funciones hash empleadas (Nº Filas en la matriz de sketch).")
-    parser.add_argument("-m", type=int, required=True, help="Valor máximo del dominio de las funciones hash (Nº de columnas de la matriz de sketch)")
-    parser.add_argument("-e", type=float, required=True, help="Valor de epsilon.")
-    parser.add_argument("-d", type=str, required=True, help='Nombre del dataset empleado')
-    parser.add_argument("--verbose_time", action="store_true", help="Se desea obtener los tiempos de ejecución de las funciones.")
+    parser = argparse.ArgumentParser(description="Private Count Mean Sketch Algorithm for frequency estimation from a known domain.")
+    parser.add_argument("-k", type=int, required=True, help="Number of hash functions used (Rows in the sketch matrix).")
+    parser.add_argument("-m", type=int, required=True, help="Maximum domain value of the hash functions (Columns in the sketch matrix).")
+    parser.add_argument("-e", type=float, required=True, help="Epsilon value.")
+    parser.add_argument("-d", type=str, required=True, help="Name of the dataset used.")
+    parser.add_argument("--verbose_time", action="store_true", help="Display execution times of the functions.")
     args = parser.parse_args()
-    dataset,df, domain = utils.load_dataset(args.d)
 
+    # Load the dataset
+    dataset, df, domain = utils.load_dataset(args.d)
 
-    PCMS = privateCMS(args.e,args.k,args.m,dataset,domain)
-    f_estimada, tiempos = PCMS.execute()
+    # Initialize the private count mean sketch
+    PCMS = privateCMS(args.e, args.k, args.m, dataset, domain)
 
+    # Client side: process the private data
+    privatized_data = PCMS.execute_client()
+
+    # Server side: process the private data
+    f_estimated = PCMS.execute_server(privatized_data)
+
+    # Show the results
     os.system('cls' if os.name == 'nt' else 'clear>/dev/null')
-    if args.verbose_time: 
-        if TEST_MODE: 
-            for t in tiempos:
-                print(f"{t[0]}: {t[1]}")
-        else:
-            tabla_tiempos = tabulate(tiempos, headers=["Algoritmo", "Tiempo de Ejecución"], tablefmt="pretty")
-            print(tabla_tiempos + '\n')
-    utils.mostrar_resultados(df,f_estimada)
 
-
-  
-# cambiar en el lado del cliente: matriz del cliente de las v y una ultima columna de j
-# 
+    utils.display_results(df, f_estimated)
--- utils/utils.py
+++ utils/utils.py
@@ -7,152 +7,127 @@ import matplotlib.pyplot as plt
 from tabulate import tabulate
 from scipy.stats import pearsonr
 
-TEST_MODE = False#(ACTIVAR para test)
+TEST_MODE = False  # (ENABLE for testing)
 
-
-def create_dataset(N:int, type:str)->tuple[list,pd.DataFrame,list]:
+def create_dataset(N: int, dist_type: str) -> tuple[list, pd.DataFrame, list]:
     """
-    Creación de un dataset para la estimación de frecuencias.
+    Creates a dataset for frequency estimation.
 
     Args:
-        N (int): Número de elementos en el dataset.
-        type (string): Tipo de generador [exp (exponencial), norm (normal), small (valores distribuidos en un dominio reducido)]
+        N (int): Number of elements in the dataset.
+        dist_type (string): Distribution type [exp (exponential), norm (normal), small (values within a reduced domain)].
 
     Returns:
-        valores (list): Dataset generado en formato lista.
-        df (Dataframe): Dataset generado en formato dataframe (libreria Pandas)
-        unique_values (list): Valores únicos (dominio) del dataset
+        values (list): Generated dataset in list format.
+        df (DataFrame): Generated dataset in Pandas DataFrame format.
+        unique_values (list): Unique values (domain) in the dataset.
 
     Examples:
         >>> create_dataset(10**6, 'exp')
         >>> create_dataset(1000, 'small')
     """
-    # Generar valores exponenciales
-    if type == 'exp':
-        valores = np.random.exponential(scale=2.0, size=N).astype(int)
-    elif type == 'norm':
-        valores = np.random.normal(loc=12, scale=2, size=N).astype(int)
-    elif type == 'small':
-        elementos = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-        frecuencias = [0.29, 0.19, 0.15, 0.12, 0.1,0.08, 0.05, 0.02]
-        dataset = np.random.choice(elementos, size=N, p=frecuencias)
-        valores = dataset.tolist()
-        np.random.shuffle(valores)
+    if dist_type == 'exp':
+        values = np.random.exponential(scale=2.0, size=N).astype(int)
+    elif dist_type == 'norm':
+        values = np.random.normal(loc=12, scale=2, size=N).astype(int)
+    elif dist_type == 'small':
+        elements = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+        frequencies = [0.29, 0.19, 0.15, 0.12, 0.1, 0.08, 0.05, 0.02]
+        dataset = np.random.choice(elements, size=N, p=frequencies)
+        values = dataset.tolist()
+        np.random.shuffle(values)
     
-    # Crear un DataFrame
-    df = pd.DataFrame({
-        'value': valores,
-    })
-
+    df = pd.DataFrame({'value': values})
     unique_values = df['value'].unique().tolist()
     unique_values.sort()
-    return valores,df, unique_values
+    return values, df, unique_values
 
 def load_dataset(csv_filename):
     """
-    Carga un dataset desde un archivo CSV y devuelve los valores, el DataFrame y los valores únicos de 'value'.
+    Loads a dataset from a CSV file and returns the values, the DataFrame, and unique 'value' entries.
 
     Args:
-    - csv_filename (str): Nombre del archivo CSV que se encuentra en la carpeta 'datasets'.
+        csv_filename (str): Name of the CSV file located in the 'datasets' folder.
 
     Returns:
-    - valores (list): Dataset generado en formato lista.
-    - df (DataFrame): Dataset generado en formato DataFrame (biblioteca Pandas).
-    - unique_values (list): Valores únicos (dominio) del dataset.
+        values (list): Dataset in list format.
+        df (DataFrame): Dataset in Pandas DataFrame format.
+        unique_values (list): Unique values (domain) of the dataset.
     """
-    # Construir la ruta completa al archivo CSV
-    dataset_path =  os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data/processed', csv_filename + '.csv'))
-
-    # Cargar el dataset desde el archivo CSV
+    dataset_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data/processed', csv_filename + '.csv'))
     df = pd.read_csv(dataset_path)
     df = df[['value']]
-    
-    # Obtener los valores como lista
-    valores = df['value'].tolist()
-    
-    # Obtener los valores únicos
+    values = df['value'].tolist()
     unique_values = df['value'].unique().tolist()
-    
-    # Devolver los resultados
-    return valores, df, unique_values
+    return values, df, unique_values
 
-def generate_hash_functions(k, p, c,m):
+def generate_hash_functions(k, p, c, m):
     """
-    Generación de un conjunto de k funciones hash (D->m) c-independientes entre si.
-        Parámetros:
-            c (int) (Número de coeficientes): Cuando se define una familia de funciones c-independientes
-            k (int) (Número de funciones hash)
-            p (int) (Número primo grande): Necesario para la construccion de las funciones hash
-            m (int) (Valor máximo del dominio al que mapean las funciones hash)
+    Generates a set of k c-independent hash functions (D -> m).
 
-        Salida:
-            hash_functions (list): Conjunto con la familia de k funciones hash
+    Args:
+        c (int): Number of coefficients for c-independent hash functions.
+        k (int): Number of hash functions.
+        p (int): Large prime number for hash function construction.
+        m (int): Maximum domain value to which the hash functions map.
+
+    Returns:
+        hash_functions (list): Set of k hash functions.
     """
     hash_functions = []
     for _ in range(k):
-        # c coeficientes aleatorios modulo p
-        coefficients = [random.randint(1, p-1) for _ in range(c)]
-        # Construimos las funciones hash como expresiones labda
-        hash_func = lambda x, coeffs=coefficients, p=p: (sum((coeffs[i] * (hash(x) ** i)) % p for i in range(c)) % p)%m
+        coefficients = [random.randint(1, p - 1) for _ in range(c)]
+        hash_func = lambda x, coeffs=coefficients, p=p: (sum((coeffs[i] * (hash(x) ** i)) % p for i in range(c)) % p) % m
         hash_functions.append(hash_func)
     return hash_functions
 
-def mostrar_resultados(frecuencia_real:pd.DataFrame, f_estimada_num: dict):
-    N = frecuencia_real.shape[0]
+def display_results(real_freq: pd.DataFrame, estimated_freq: dict):
+    """
+    Displays and compares real and estimated frequencies, along with error metrics.
 
-    # Cálculos relacionados con las frecuencias reales
-    f = frecuencia_real['value'].value_counts()
-    f_real_num = (f.sort_index()).to_dict()
-    f_real_percent = ((f*100/N).sort_index()).to_dict()
-    
-    # Preparar los datos para su posterior impresión por pantalla
-    tabla_datos = []
-    for elemento in f_real_num:
-        if elemento in f_estimada_num:
-            frec_real = f_real_num[elemento]
-            porc_real = f_real_percent[elemento]
-            frec_estimada = f_estimada_num[elemento]
-            porc_estimada = (frec_estimada /N) * 100
-            diff = abs(f_real_num[elemento] - f_estimada_num[elemento])
-            tabla_datos.append([elemento, frec_real, f"{porc_real:.3f}%", f"{frec_estimada:.2f}", f"{porc_estimada:.3f}%",f"{diff:.2f}"])
-    
+    Args:
+        real_freq (DataFrame): Real frequency data.
+        estimated_freq (dict): Estimated frequency data.
+    """
+    N = real_freq.shape[0]
+    f = real_freq['value'].value_counts()
+    real_num_freq = f.sort_index().to_dict()
+    real_percent_freq = ((f * 100 / N).sort_index()).to_dict()
 
-    # Cálculo de errores
-    errores = [abs((f_real_num[key] - f_estimada_num[key])) for key in f_estimada_num]
-    errores_mean = np.mean(errores)
-    errores = np.sum(errores)
-    max_f = max(f_real_num.values())
-    min_f = min(f_real_num.values())
+    data_table = []
+    for element in real_num_freq:
+        if element in estimated_freq:
+            real_count = real_num_freq[element]
+            real_percent = real_percent_freq[element]
+            estimated_count = estimated_freq[element]
+            estimated_percent = (estimated_count / N) * 100
+            diff = abs(real_num_freq[element] - estimated_freq[element])
+            data_table.append([element, real_count, f"{real_percent:.3f}%", f"{estimated_count:.2f}", f"{estimated_percent:.3f}%", f"{diff:.2f}"])
 
-    mse = np.sum([(f_real_num[key] - f_estimada_num[key])**2 for key in f_estimada_num])/len(f_estimada_num)
-    mse_norm = mse/(max_f-min_f)#np.sum([(f_real_num[key]/N - f_estimada_num[key]/N)**2 for key in f_estimada_num])/len(f_estimada_num)
-    coef_pearson, _ = pearsonr(list(f_real_num.values()),list(f_estimada_num.values()))
+    errors = [abs(real_num_freq[key] - estimated_freq[key]) for key in estimated_freq]
+    mean_error = np.mean(errors)
+    total_errors = np.sum(errors)
+    max_freq = max(real_num_freq.values())
+    min_freq = min(real_num_freq.values())
+    mse = np.sum([(real_num_freq[key] - estimated_freq[key]) ** 2 for key in estimated_freq]) / len(estimated_freq)
+    normalized_mse = mse / (max_freq - min_freq)
+    pearson_corr, _ = pearsonr(list(real_num_freq.values()), list(estimated_freq.values()))
 
-    errores = [['Numero de errores', str("{:.2f}".format(errores))],['Numero de errores (media)', str("{:.2f}".format(errores_mean))],['Error porcentual', str("{:.2f}".format((errores_mean/N)*100) + '%')],['MSE', str("{:.2f}".format((mse)))], ['RMSE', str("{:.2f}".format((np.sqrt(mse))))],['MSE (Normalizado)', str("{:.2f}".format((mse_norm)))], ['RMSE (Normalizado)', str("{:.2f}".format((np.sqrt(mse_norm))))], ['Coeficiente correlacion Pearson', str("{:.4f}".format(coef_pearson))]]
+    error_table = [
+        ['Total Errors', f"{total_errors:.2f}"],
+        ['Mean Error', f"{mean_error:.2f}"],
+        ['Percentage Error', f"{(mean_error / N) * 100:.2f}%"],
+        ['MSE', f"{mse:.2f}"],
+        ['RMSE', f"{np.sqrt(mse):.2f}"],
+        ['Normalized MSE', f"{normalized_mse:.2f}"],
+        ['Normalized RMSE', f"{np.sqrt(normalized_mse):.2f}"],
+        ['Pearson Correlation Coefficient', f"{pearson_corr:.4f}"],
+    ]
 
     if TEST_MODE:
-        errores = [['Media de errores', str("{:.2f}".format(errores_mean))],['Error porcentual', str("{:.2f}".format((errores_mean/N)*100) + '%')],['MSE', str("{:.2f}".format((mse)))], ['RMSE', str("{:.2f}".format((np.sqrt(mse))))],['MSE (Normalizado)', str("{:.2f}".format((mse_norm)))], ['RMSE (Normalizado)', str("{:.2f}".format((np.sqrt(mse_norm))))], ['Coeficiente correlacion Pearson', str("{:.4f}".format(coef_pearson))]]
-        for error in errores:
+        for error in error_table:
             print(f"{error[0]}: {error[1]}")
     else:
-        print("RESULTADOS OBTENIDOS")
-        print(tabulate(tabla_datos, headers=["Elemento", "Frecuencia Real", "Porcentaje Real", "Frecuencia Estimada", "Porcentaje Estimado", "Diferencia en la estimacion"], tablefmt="pretty"))
-        
-        tabla_errores = tabulate(errores, tablefmt="pretty")
-        print('\n'+tabla_errores)
-
-        # Representación visual
-        data = {
-            'Frecuencia Real': f_real_num,
-            'Frecuencia Estimada': f_estimada_num
-        }
-        df = pd.DataFrame(data)
-        
-        # # Crear la gráfica
-        # df.plot(kind='bar', figsize=(10, 6))
-        # plt.title('Comparación de Frecuencias Reales y Estimadas')
-        # plt.xlabel('Elementos')
-        # plt.ylabel('Número de Ocurrencias')
-        # plt.xticks(rotation=0)
-        # plt.legend(loc='best')
-        # #plt.show() 
+        print("RESULTS")
+        print(tabulate(data_table, headers=["Element", "Real Frequency", "Real Percentage", "Estimated Frequency", "Estimated Percentage", "Estimation Difference"], tablefmt="pretty"))
+        print('\n' + tabulate(error_table, tablefmt="pretty"))
